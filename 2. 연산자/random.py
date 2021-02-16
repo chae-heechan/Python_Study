3f6bae709c9313670f56b000413e0bed6837e656
Chae <eqholic1125@gmail.com>
2021-02-16 20:50:16 +0900

    2021-02-16

--- 2. 연산자/random.py
+++ 2. 연산자/random.py
@@ -1,3 +1,27 @@
 from random import *
 
-print(random())
+print(random()) # 0.0. 이상 1.0. 미만의 임의의 값을 생성
+print(random() * 10) # 0.0 이상 10.0 미만의 임의의 값을 생성
+print(int(random() * 10)) # 0 이상 10 미만의 임의의 값을 생성
+print(int(random() * 10)) # 0 이상 10 미만의 임의의 값을 생성
+print(int(random() * 10)) # 0 이상 10 미만의 임의의 값을 생성
+print(int(random() * 10) + 1) # 1 이상 10 이하의 임의의 값을 생성
+print(int(random() * 10) + 1) # 1 이상 10 이하의 임의의 값을 생성
+print(int(random() * 10) + 1) # 1 이상 10 이하의 임의의 값을 생성
+print(int(random() * 10) + 1) # 1 이상 10 이하의 임의의 값을 생성
+
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+print(int(random() * 45) + 1) # 1 이상 45이하의 임의의 값을 생성
+
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+print(randrange(1, 46)) # 1 이상 46 미만의 임의의 값을 생성
+
+print(randint(1, 45)) # 1 ~ 45 임의의 값을 생성
