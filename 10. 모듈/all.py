fe76a9bf3b344099f42c5637ca5a6d8b887ac4d1
채희찬 <eqholic1125@gmail.com>
2021-04-24 00:01:57 +0900

    2021-04-23

---
+++ 10. 모듈/all.py
@@ -0,0 +1,16 @@
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThailandPackage
+# trip_to = ThailandPackage()
+# trip_to.detail()
+
+# from travel import veitnam
+# trip_to = veitnam.VeitnamPackage()
+# trip_to.detail()
+
+from travel import *
+# trip_to = veitnam.VeitnamPackage()
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
