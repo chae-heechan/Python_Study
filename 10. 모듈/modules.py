981c3e73e476f01814602843fbc701b0a7f4f8da
Chae hee chan <eqholic1125@gmail.com>
2021-05-09 20:12:59 +0900

    codeup 6005

--- 10. 모듈/modules.py
+++ 10. 모듈/modules.py
@@ -20,7 +20,7 @@
 # print(time.strftime("%Y-%m-%d %H:%M:%S"))
 
 import datetime
-# print("오늘 날짜는 ", datetime.date.today())
+print("오늘 날짜는 ", datetime.date.today())
 today = datetime.date.today()
 td = datetime.timedelta(days=100)
 print("우리가 만난지 100일은 ", today + td)
---
+++ CodeUp/6005.py
@@ -0,0 +1,2 @@
+# 출력하기 05
+print('"Hello World"')
